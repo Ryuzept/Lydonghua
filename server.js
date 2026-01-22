695f2ccdca715ba0285f3646e0c5b00b205d381f
Ryuzept <dannd5119@gmail.com>
2026-01-22 11:00:57 +0700

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-import http from "http";
-import app from "./app";
-import { initNobarSocket } from "./socket/nobar";
-
-const server = http.createServer(app);
-initNobarSocket(server);
-
-server.listen(3000);
